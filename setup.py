a7c8ceedf0ca234c4f0448c90be87ce1f3e6159e
Tony Kamillo (Sacanix) <tonysacanix@gmail.com>
2013-07-16 10:34:31 -0300

    Adding license and installer. Improving README.

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Tweet-Command-Line-Tool
+-------------
+
+Simple python script to send tweets. It can be ease integrated to another app, such as media player, to send Now Playing status.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Tweet-Command-Line-Tool',
+    version='0.5',
+    url='http://github.com/sacanix/Flask-Nytro',
+    license='BSD',
+    author='Tony Kamillo (Sacanix)',
+    author_email='tonysacanix@gmail.com',
+    description='''Simple python script to send tweets. It can be ease integrated to another app, such as media player, to send Now Playing status.''',
+    long_description=__doc__,
+    # if you would be using a package instead use packages instead
+    # of py_modules:
+    py_modules=['tweet-tool'],
+    #packages=[],
+    #zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['python-twitter'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Twiter'
+    ]
+)
